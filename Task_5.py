de183b57b437fcf6776903b59fcb8c3c9da32661
Aleksandr Shelpov <shelpov-a@mail.ru>
2020-01-27 23:40:29 +0300

    Reshaped task #5

--- Task_5.py
+++ Task_5.py
@@ -4,40 +4,50 @@
 в рейтинге существуют элементы с одинаковыми значениями, то новый элемент с тем же
 значением должен разместиться после них.
 """
-while True:
-    user_list_of_ratings = input("Enter list of initial ratings (must be integers), divided by gap : ").split(" ")
-    try:
-        initial_list_of_ratings = []
-        for i in user_list_of_ratings:
-            initial_list_of_ratings.append(int(i))
-        initial_list_of_ratings.sort(reverse=True)
-    except ValueError:
-        print()
-        print("You enter a string or a float or something else.\nTry again please.")
-        print("="*100)
-        continue
-    else:
-        break
 
-print("="*100)
-print("Now you will add additional rating numbers, it must be only string and only one per iteration.\n"
-      "If you want to stop adding new items type 'Stop', this will end program and return final list of ratings.")
-
-final_list_of_ratings = initial_list_of_ratings
+final_list_of_ratings = []
 while True:
-    user_command = input("Please enter new rating. It must be integer and one per time: ")
+    user_command = input("Please enter new rating. It must be integer and one per time.\n"
+                         "If you want stop program enter 'Stop': ")
     print("-"*100)
     if user_command.lower() == "stop":
         print("Your final list of ratings is: ", *final_list_of_ratings)
         print("Program have been ended")
         print("*"*100)
         break
+    # accept only integers and only one per iteration
     try:
-        user_command = int(user_command)
-        final_list_of_ratings.append(user_command)
-        final_list_of_ratings.sort(reverse=True)
-        print("Your current list of ratings is: ", *final_list_of_ratings)
-        print("="*100)
+        user_number = abs(int(user_command))
+        # if list of ratings is empty
+        if len(final_list_of_ratings) == 0:
+            final_list_of_ratings.append(user_number)
+        # if only one record in list of rating
+        elif len(final_list_of_ratings) == 1:
+            if final_list_of_ratings[0] > user_number:
+                final_list_of_ratings.append(user_number)
+            else:
+                final_list_of_ratings.insert(0, user_number)
+        # for two and more records in list of rating
+        else:
+            # if rating has such value in list of ratings
+            if user_number in final_list_of_ratings:
+                new_index = final_list_of_ratings.index(user_number) + final_list_of_ratings.count(user_number)
+                final_list_of_ratings.insert(new_index, user_number)
+            # if value of rating new in list of ratings
+            else:
+                # for circumstances when new number is greater than all in list of ratings
+                if final_list_of_ratings[0] < user_number:
+                    final_list_of_ratings.insert(0, user_number)
+                # for circumstances when new number is less than all in list of ratings
+                elif final_list_of_ratings[-1] > user_number:
+                    final_list_of_ratings.append(user_number)
+                # for circumstances when new number between numbers in space of list of integers
+                else:
+                    for i in range(len(final_list_of_ratings)):
+                        if final_list_of_ratings[i] > user_number > final_list_of_ratings[i + 1]:
+                            new_index = i + 1 + final_list_of_ratings.count(user_number)
+                            final_list_of_ratings.insert(new_index, user_number)
+                            break
     except ValueError:
         print()
         print("You enter a string or a float or something else, but not an integer.\nTry again please.\n"
