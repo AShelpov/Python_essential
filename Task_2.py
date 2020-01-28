fa788c9c48e2f14e133a1c322f77236d23c06584
Aleksandr Shelpov <shelpov-a@mail.ru>
2020-01-28 16:47:55 +0300

    Have done tasks from 1 to 3

---
+++ Task_2.py
@@ -0,0 +1,23 @@
+def func_for_naming(name, surname, birth_year, city_of_accomoodation,
+                     e_mail, phone_number, **kwargs):
+    result = f"Full name of user: {name} {surname}, year of birth: {birth_year}, current location: {city_of_accomoodation}," \
+        f" contact phone: {phone_number}, email: {e_mail}."
+    if len(kwargs):
+        result += " Other features is - "
+        i = 0
+        for key, value in kwargs.items():
+            result += str(key)
+            result += ": "
+            result += str(value)
+            # computing comma or dot in the end of statement.
+            i += 1
+            if i == len(kwargs):
+                result += "."
+            else:
+                result += ", "
+    print(result)
+
+
+
+
+
---
+++ Task_3.py
@@ -0,0 +1,22 @@
+def my_func(a=0, b=0, c=0, *args):
+    """
+    Function expect three argument and return the sum of the two greatest of them.
+    If would be passed less then three arguments, function will estimate missing as null.
+    Function can accept more than three arguments, but returns sum of the two greatest considered additional values
+    Function expect arguments as integer or float. But it won't raise error if transfer other types.
+    It will consider such argument as null
+    :param a: int or float
+    :param b: int or float
+    :param c: int or float
+    :param args: int or float
+    :return: int or float
+    """
+    expected_values = [a if isinstance(a, (int, float)) else 0,
+                       b if isinstance(b, (int, float)) else 0,
+                       c if isinstance(c, (int, float)) else 0]
+    unexpected_values = [i for i in args if isinstance(i, (int, float))]
+    computing_list = expected_values + unexpected_values
+    computing_list = sorted(computing_list, reverse=True)
+
+    return computing_list[0] + computing_list[1]
+
