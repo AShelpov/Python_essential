a6c1bd4d2367804ad1ac66f233aeeee14a5336e6
Aleksandr Shelpov <shelpov-a@mail.ru>
2020-02-04 00:38:04 +0300

    Have done tasks 1 and 2

---
+++ Task_2.py
@@ -0,0 +1,22 @@
+"""
+Представлен список чисел. Необходимо вывести элементы исходного списка, значения
+которых больше предыдущего элемента.
+Подсказка: элементы, удовлетворяющие условию, оформить в виде списка. Для
+формирования списка использовать генератор.
+Пример исходного списка: [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55].
+Результат: [12, 44, 4, 10, 78, 123].
+"""
+user_numbers = input("Enter numbers divided by comma: ").split(",")
+list_of_numbers = []
+
+for i in user_numbers:
+    try:
+        list_of_numbers.append(float(i))
+    except ValueError:
+        print(f"One of entered symbols is not a number")
+
+output_list = [list_of_numbers[i + 1] for i in range(len(list_of_numbers) - 1)\
+               if list_of_numbers[i + 1] > list_of_numbers[i]]
+
+print(list_of_numbers)
+print(output_list)
